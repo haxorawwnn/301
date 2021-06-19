ceadd4e22c218be9215c3f6ddfd6a06c017563fc
haxorawwnn <85847935+haxorawwnn@users.noreply.github.com>
2021-06-19 15:56:38 +0500

    Add files via upload

---
+++ 301.py
@@ -0,0 +1,62 @@
+#This Code is Written by Sachin Shrivastv
+#coding = utf-8
+
+import os, time, sys
+
+os.system('clear')
+
+def printing(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.001)
+logo=''' 
+
+                 ,----.         ,-.-. .=-.-.          
+  _,..---._   ,-.--` , \ ,--.-./=/ ,//==/_ / _.-.     
+/==/,   -  \ |==|-  _.-`/==/, ||=| -|==|, |.-,.'|     
+|==|   _   _\|==|   `.-.\==\,  \ / ,|==|  |==|, |     
+|==|  .=.   /==/_ ,    / \==\ - ' - /==|- |==|- |     
+|==|,|   | -|==|    .-'   \==\ ,   ||==| ,|==|, |     
+|==|  '='   /==|_  ,`-._  |==| -  ,/|==|- |==|- `-._  
+|==|-,   _`//==/ ,     /  \==\  _ / /==/. /==/ - , ,/ 
+`-.`.____.' `--`-----``    `--`--'  `--`-``--`-----'  
+
+ 
+logo2='''  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+ 
+def login():
+	print(logo)
+	print(logo2)
+	print('                     Owner:-    Awanhaxor')
+	print('                     Author:-   Shahwaiz')
+	print('                     Whatsapp:- +923453375398')
+	print(logo2)
+	username = raw_input('  Username:- ')
+	if username =='':
+		print(' [!] Invalid Username')
+		time.sleep(1)
+		os.system('clear')
+		login()
+	elif username =='Haxor':
+		password = raw_input('  Password:- ')
+		if password =='':
+			print(' [!] Invalid Password')
+			time.sleep(1)
+			os.system('clear')
+			login()
+		elif password =='Awam':
+			printing('  Logged Successful ')
+			os.system('python2 base.py')
+		else:
+			print(' [!] Invalid Password')
+			time.sleep(1)
+			os.system('clear')
+			login()
+	else:
+		print(' [!] Invalid Username')
+		time.sleep(1)
+		os.system('clear')
+		login()
+		
+login()
